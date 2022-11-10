3f727f24786cb5c020c73d47db931524676c90d0
Eduardo Domingos <eduardo.domingos.pb@compasso.com.br>
2022-11-10 16:31:49 -0300

    Automated fulfillment of sandwich items type finished.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const requestUrl = "https://personal-27rwjmv9.outsystemscloud.com/cardapio/rest/lanches/list"
 
 async function getData(url) { //GET Request to endpoint, return response json
-	const response = await fetch(requestUrl);
+	const response = await fetch(url);
 	// Handle errors
 	if (!response.ok) {
 		throw new Error(`HTTP error! status: ${response.status}`);
@@ -11,9 +11,41 @@ async function getData(url) { //GET Request to endpoint, return response json
   return data
 }
 
-async function main() { //Main function where API response will be used
+function createList(data) { //Creating list dynamically
+	let rightCategory = 0
+
+	switch (data.CategoryId) {
+		case 2:
+			rightCategory = document.querySelector("#dois")
+			break;
+	
+		default:
+			break;
+	}
+
+	const li = document.createElement("li")
+	
+	li.innerHTML = `
+		<div class="details">
+			<h3>${data.name}</h3>
+			<p class="description">
+				${data.ingredients}
+			</p>
+		</div>
+		<strong class="price">
+			R$${data.price}
+		</strong>
+	`
+	rightCategory.appendChild(li)
+}
+
+async function main() { //Main function where API response will be used to fill lists
   const data = await getData(requestUrl)
   console.log(data)
+
+	data.forEach(element => {
+    createList(element)    
+  });
 }
 
 main()
